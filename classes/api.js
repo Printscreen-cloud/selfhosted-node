ef2b7636db04d759137d0e68eb49a2e20dbff392
KiwontaTv <dominic-zahn@web.de>
2025-10-23 20:54:46 +0200

    push version as txt file

--- classes/api.js
+++ classes/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 class API {
 	call(path, data = {}) {
 		return new Promise((resolve, reject) => {
-			axios.post(`https://api.printscreen.cloud/${path}`, data)
+			axios.post(process.env.dev ? `http://localhost:3000/${path}` : `https://api.printscreen.cloud/${path}`, data)
 				.then(response => {
 					resolve(response.data);
 				})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logs from './classes/logs.js';
 import logic from './classes/logic.js';
+import fs from 'fs';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,7 +9,7 @@ const port = process.env.PORT || 3001;
 // Define API keys
 const auth = process.env.AUTH || "9Yw1-asLyCxyikksejiGu";
 const secret = process.env.SECRET || "2ba7c7c7146f290bb34e82a640f22d22:add30cdf8cd9f4da3c5c3e9a7d9ad55871b01366b646b3d54127c81feb150c71";
-const version = "1.0.0";
+const version = fs.readFileSync('./version.txt', 'utf8').trim();
 
 await logic.setup(app);
 logic.loop(auth, secret, version);
