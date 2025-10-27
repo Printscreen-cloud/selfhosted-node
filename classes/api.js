2c302d3664934ef93b0c9b7891613df5396b3427
KiwontaTv <dominic-zahn@web.de>
2025-10-27 09:21:58 +0100

    fix: url

--- classes/api.js
+++ classes/api.js
@@ -6,7 +6,8 @@ dotenv.config();
 class API {
 	call(path, data = {}) {
 		return new Promise((resolve, reject) => {
-			axios.post(process.env.dev ? `http://localhost:3000/${path}` : `https://api.printscreen.cloud/${path}`, data)
+			const url = process.env.dev ? (process.env.url ? process.env.url + `/${path}` : `http://localhost:3000/${path}`) : `https://api.printscreen.cloud/${path}`;
+			axios.post(url, data)
 				.then(response => {
 					resolve(response.data);
 				})
