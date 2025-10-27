32d906f5159e00bc310522752483e40402a8fa0e
KiwontaTv <dominic-zahn@web.de>
2025-10-27 09:33:11 +0100

    temp ip

--- classes/api.js
+++ classes/api.js
@@ -6,8 +6,7 @@ dotenv.config();
 class API {
 	call(path, data = {}) {
 		return new Promise((resolve, reject) => {
-			const url = process.env.dev ? (process.env.url ? process.env.url + `/${path}` : `http://localhost:3000/${path}`) : `https://api.printscreen.cloud/${path}`;
-			axios.post(url, data)
+			axios.post(process.env.dev ? `http://172.29.128.1:3000/${path}` : `https://api.printscreen.cloud/${path}`, data)
 				.then(response => {
 					resolve(response.data);
 				})
